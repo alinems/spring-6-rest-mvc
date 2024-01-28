00af51bbe5a7e537970320c6a6784eccca0e70c4
alinems <5702162+alinems@users.noreply.github.com>
2024-01-28 19:16:38 +0100

    customer controller IT

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -0,0 +1,38 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
+import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class CustomerControllerIT {
+    @Autowired
+    CustomerController customerController;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testCustomerIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            customerController.getCustomerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testCustomerById() {
+        Customer customer = customerRepository.findAll().get(0);
+
+        CustomerDTO dto = customerController.getCustomerById(customer.getId());
+
+        assertThat(dto).isNotNull();
+    }
+}
