08c06ff83840df8132a6090f654f7afe6c6bac56
alinems <5702162+alinems@users.noreply.github.com>
2025-02-12 18:32:18 +0100

    update to UuidGenerator + init tests

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/MySqlTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("localmysql")
+public class MySqlTest {
+
+    @Container
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:9");
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<Beer> beers = beerRepository.findAll();
+
+        assertThat(beers.size()).isGreaterThan(0);
+    }
+}
