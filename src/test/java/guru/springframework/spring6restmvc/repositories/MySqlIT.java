1d5f37ab30f7a977e15822bd0cc764b6e2591faf
alinems <5702162+alinems@users.noreply.github.com>
2025-02-13 17:55:47 +0100

    update to UuidGenerator + init tests

--- src/test/java/guru/springframework/spring6restmvc/repositories/MySqlIT.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/MySqlIT.java
@@ -4,26 +4,27 @@ import guru.springframework.spring6restmvc.entities.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Created by jt, Spring Framework Guru.
+ */
 @Testcontainers
 @SpringBootTest
 @ActiveProfiles("localmysql")
-public class MySqlTest {
+public class MySqlIT {
 
     @Container
+    @ServiceConnection
     static MySQLContainer<?> mySQLContainer = new MySQLContainer<>("mysql:9");
-
-    @Autowired
-    DataSource dataSource;
 
     @Autowired
     BeerRepository beerRepository;
